d3577529bd3432ebab61a0d54bff3cbf890653d4
maxence.moutoussamy1 <maxence.moutoussamy1@gmail.com>
2019-08-30 18:57:12 +0200

    use style and add the delete feature

--- src/Main.elm
+++ src/Main.elm
@@ -23,6 +23,7 @@ type alias Model =
 type Msg
     = ChangeInput String
     | AddTodo
+    | RemoveTodo Int
 
 
 init : Model
@@ -46,11 +47,33 @@ update msg model =
                 , todos = model.todos ++ [ model.input ]
             }
 
+        RemoveTodo i ->
+            let
+                before =
+                    List.take i model.todos
+
+                after =
+                    List.drop (i + 1) model.todos
+            in
+            { model
+                | todos = before ++ after
+            }
+
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] <| List.map (div [] << List.singleton << text) model.todos
-        , input [ placeholder "Enter a todo item", value model.input, onInput ChangeInput ] []
-        , button [ onClick AddTodo ] [ text "Add" ]
+    div [ class "todos" ]
+        [ div [] <| List.indexedMap viewTodo model.todos
+        , div [ class "input" ]
+            [ input [ placeholder "Enter a todo item", value model.input, onInput ChangeInput ] []
+            , button [ onClick AddTodo ] [ text "Add" ]
+            ]
+        ]
+
+
+viewTodo : Int -> String -> Html Msg
+viewTodo index value =
+    div [ class "todo" ]
+        [ div [] [ text value ]
+        , button [ onClick <| RemoveTodo index ] [ text "X" ]
         ]
