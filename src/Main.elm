f5678ef4f9c8bc619a648fe0cf42d28497618b82
Maxence Moutoussamy <maxence.moutoussamy@ackl.io>
2019-09-04 16:59:17 +0200

    use material theme

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,22 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html exposing (Html, div, text)
+import Material
+import Material.Card as Card
+import Material.Fab as Fab
+import Material.LayoutGrid as LayoutGrid
+import Material.List as Lists
+import Material.Options as Options exposing (css, styled)
+import Material.TextField as TextField
+import Material.Typography as Typography
 
 
+main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
+        , subscriptions = subscriptions
         , update = update
         , view = view
         }
@@ -17,35 +25,48 @@ main =
 type alias Model =
     { input : String
     , todos : List String
+    , mdc : Material.Model Msg
     }
 
 
 type Msg
-    = ChangeInput String
+    = Mdc (Material.Msg Msg)
+    | ChangeInput String
     | AddTodo
     | RemoveTodo Int
 
 
-init : Model
-init =
-    { input = ""
-    , todos = []
-    }
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    ( { input = ""
+      , todos = []
+      , mdc = Material.defaultModel
+      }
+    , Material.init Mdc
+    )
 
 
-update : Msg -> Model -> Model
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Material.subscriptions Mdc model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Mdc msg_ ->
+            Material.update Mdc msg_ model
+
         ChangeInput str ->
-            { model
-                | input = str
-            }
+            ( { model | input = str }, Cmd.none )
 
         AddTodo ->
-            { model
+            ( { model
                 | input = ""
                 , todos = model.todos ++ [ model.input ]
-            }
+              }
+            , Cmd.none
+            )
 
         RemoveTodo i ->
             let
@@ -55,25 +76,58 @@ update msg model =
                 after =
                     List.drop (i + 1) model.todos
             in
-            { model
-                | todos = before ++ after
-            }
+            ( { model | todos = before ++ after }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "todos" ]
-        [ div [] <| List.indexedMap viewTodo model.todos
-        , div [ class "input" ]
-            [ input [ placeholder "Enter a todo item", value model.input, onInput ChangeInput ] []
-            , button [ onClick AddTodo ] [ text "Add" ]
+    Card.view [ css "max-width" "500px", css "margin" "24px" ]
+        [ styled div
+            [ css "padding" "1rem" ]
+            [ styled Html.h3 [ Typography.title, css "margin" "0" ] [ text "To do list" ]
+            , model.todos
+                |> List.indexedMap viewTodo
+                |> Lists.ul Mdc "todo-list" model.mdc [ Lists.nonInteractive ]
+            , LayoutGrid.view [ css "padding" "8px" ]
+                [ LayoutGrid.cell [ LayoutGrid.span9 ]
+                    [ TextField.view Mdc
+                        "todo-input"
+                        model.mdc
+                        [ TextField.label "Task label"
+                        , TextField.value model.input
+                        , TextField.placeholder "Enter your task..."
+                        , TextField.fullwidth
+                        , Options.onInput ChangeInput
+                        ]
+                        []
+                    ]
+                , LayoutGrid.cell [ LayoutGrid.span3 ]
+                    [ Fab.view Mdc
+                        "add-button"
+                        model.mdc
+                        [ Fab.extended
+                        , Fab.icon "add"
+                        , Fab.label
+                        , Options.onClick AddTodo
+                        ]
+                        [ text "Add" ]
+                    ]
+                ]
             ]
         ]
 
 
-viewTodo : Int -> String -> Html Msg
+viewTodo : Int -> String -> Lists.ListItem Msg
 viewTodo index value =
-    div [ class "todo" ]
-        [ div [] [ text value ]
-        , button [ onClick <| RemoveTodo index ] [ text "X" ]
+    Lists.li []
+        [ Lists.text []
+            [ Lists.primaryText [] [ text value ]
+            , Lists.secondaryText []
+                [ index
+                    |> String.fromInt
+                    |> (++) "Item #"
+                    |> text
+                ]
+            ]
+        , Lists.metaIcon [ Options.onClick <| RemoveTodo index ] "clear"
         ]
